7144df211199393d05fe46ff226f8f358a73f4fd
beomhak <qjagkrdldi@gmail.com>
2022-05-27 16:43:58 +0900

    countup_try

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function App() {
+
+  const [Count, setCount] = useState(0);
+
+  const countff = ()=>{ 
+    setCount(prev=>prev+1)
+  }
+  const countff1 = ()=>{ 
+    setCount(prev=>prev-1)
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <button onClick={ countff1 }>-</button>
+      <span>{Count}</span>
+      <button onClick={ countff }>+</button>
+    </div>
   )
 }
